16c39713cac733edd72616a3e16ec82e169aa0f8
SamuelGlz10 <a01412958@tec.mx>
2025-06-12 11:35:16 -0600

    Pruebas

--- FordFulkerson.h
+++ FordFulkerson.h
@@ -1,12 +1,14 @@
 /*
- * ActIntegradora2 - Ford-Fulkerson Algorithm Implementation
- * Copyright (C) 2024 SamyG
+ * SonarQube, open source software quality management tool.
+ * Copyright (C) 2008-2013 SonarSource
+ * http://github.com/SonarOpenCommunity/sonar-cxx
  *
- * This file is part of ActIntegradora2 and is distributed under the terms of the
- * GNU Lesser General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
+ * SonarQube is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
  *
- * ActIntegradora2 is distributed in the hope that it will be useful,
+ * SonarQube is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
